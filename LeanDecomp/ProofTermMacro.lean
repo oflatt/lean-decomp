84c5a577fae9aceaeb075872612d01d0d7d28645
oflatt <oflatt@gmail.com>
2025-12-15 11:49:57 -0800

    trying out some things

--- LeanDecomp/ProofTermMacro.lean
+++ LeanDecomp/ProofTermMacro.lean
@@ -22,42 +22,8 @@ private def containsSubstring (haystack needle : String) : Bool :=
 private def isAuxiliaryProofName (name : Name) : Bool :=
   containsSubstring name.toString "_proof_"
 
-private partial def expandAuxiliaryProofsAux (env : Environment) (visited : NameSet)
-    (e : Expr) : MetaM Expr := do
-  match e with
-  | .const name lvls =>
-      if visited.contains name || !isAuxiliaryProofName name then
-        return e
-      else
-        match env.find? name with
-        | some decl =>
-            match decl.value? with
-            | some val =>
-                let body := val.instantiateLevelParams decl.levelParams lvls
-                expandAuxiliaryProofsAux env (visited.insert name) body
-            | none => return e
-        | none => return e
-  | .app fn arg =>
-      return .app (← expandAuxiliaryProofsAux env visited fn)
-        (← expandAuxiliaryProofsAux env visited arg)
-  | .lam n ty body bi =>
-      return .lam n (← expandAuxiliaryProofsAux env visited ty)
-        (← expandAuxiliaryProofsAux env visited body) bi
-  | .forallE n ty body bi =>
-      return .forallE n (← expandAuxiliaryProofsAux env visited ty)
-        (← expandAuxiliaryProofsAux env visited body) bi
-  | .letE n ty val body nonDep =>
-      return .letE n (← expandAuxiliaryProofsAux env visited ty)
-        (← expandAuxiliaryProofsAux env visited val)
-        (← expandAuxiliaryProofsAux env visited body) nonDep
-  | .mdata md body =>
-      return .mdata md (← expandAuxiliaryProofsAux env visited body)
-  | .proj type idx body =>
-      return .proj type idx (← expandAuxiliaryProofsAux env visited body)
-  | _ => return e
-
 private def expandAuxiliaryProofs (e : Expr) : MetaM Expr := do
-  expandAuxiliaryProofsAux (← getEnv) NameSet.empty e
+  Meta.deltaExpand e isAuxiliaryProofName
 
 
 /--
@@ -77,7 +43,6 @@ elab "showProofTerm " thm:ident : command => do
     let expandedValue ← expandAuxiliaryProofs value
     let proofFmt ← Meta.ppExpr expandedValue
     let typeFmt ← Meta.ppExpr decl.type
-    logInfo m!"proof term for {thm}:\n{proofFmt}"
     let typeStr := typeFmt.pretty
     let proofStr := proofFmt.pretty
     Lean.Meta.Tactic.TryThis.addSuggestion (← getRef)
--- LeanDecomp/bigstep.lean
+++ LeanDecomp/bigstep.lean
@@ -45,6 +45,60 @@ theorem cases_if_of_true {B S T s t} (hcond : B s) : (ifThenElse B S T, s) ==> t
 theorem cases_if_of_false {B S T s t} (hcond : ¬ B s) : (ifThenElse B S T, s) ==> t → (T, s) ==> t := by
   grind
 
+
+#print cases_if_of_true
+
+theorem cases_if_of_true2 : ∀ {B : State → Prop} {S T : Stmt} {s t : State}, B s → (ifThenElse B S T,s) ==> t → (S,s) ==> t :=
+  fun {B} {S T} {s t} hcond h =>
+  Classical.byContradiction fun h_1 =>
+    id
+      (BigStep.casesOn (motive := fun a a_1 a_2 x => ifThenElse B S T = a → s = a_1 → t = a_2 → h ≍ x → False) h
+        (fun s_1 =>
+          Lean.Grind.intro_with_eq (ifThenElse B S T = skip) False (s = s_1 → t = s_1 → h ≍ BigStep.skip s_1 → False)
+            (eq_false' fun h => False.elim (noConfusion_of_Nat Stmt.ctorIdx h)) fun h_2 =>
+            False.casesOn (fun x => s = s_1 → t = s_1 → h ≍ BigStep.skip s_1 → False) h_2)
+        (fun x a s_1 =>
+          Lean.Grind.intro_with_eq (ifThenElse B S T = assign x a) False
+            (s = s_1 → (t = fun v => if v = x then a s_1 else s_1 v) → h ≍ BigStep.assign x a s_1 → False)
+            (eq_false' fun h => False.elim (noConfusion_of_Nat Stmt.ctorIdx h)) fun h_2 =>
+            False.casesOn
+              (fun x_1 => s = s_1 → (t = fun v => if v = x then a s_1 else s_1 v) → h ≍ BigStep.assign x a s_1 → False)
+              h_2)
+        (fun {S_1 T_1} {s_1 t_1 u} hS hT =>
+          Lean.Grind.intro_with_eq (ifThenElse B S T = S_1;; T_1) False
+            (s = s_1 → t = u → h ≍ BigStep.seq hS hT → False)
+            (eq_false' fun h => False.elim (noConfusion_of_Nat Stmt.ctorIdx h)) fun h_2 =>
+            False.casesOn (fun x => s = s_1 → t = u → h ≍ BigStep.seq hS hT → False) h_2)
+        (fun {B_1} {s_1 t_1} hcond_1 S_1 T_1 hbody h_2 =>
+          Stmt.ifThenElse.noConfusion h_2 fun a_eq a_eq_1 a_eq_2 h_3 h_4 =>
+            False.elim
+              (Eq.mp
+                (Eq.trans
+                  (Eq.trans (Eq.symm (eq_true hbody))
+                    (congr (congr (congrArg BigStep (Eq.symm a_eq_1)) (Eq.symm h_3)) (Eq.symm h_4)))
+                  (eq_false h_1))
+                True.intro))
+        (fun {B_1} {s_1 t_1} hcond_1 S_1 T_1 hbody h_2 =>
+          Stmt.ifThenElse.noConfusion h_2 fun a_eq a_eq_1 a_eq_2 h_3 =>
+            False.elim
+              (Eq.mp
+                (Eq.trans
+                  (Eq.trans (Eq.symm (eq_true hcond))
+                    (eq_of_heq (a_eq ▸ (fun a a' e_1 => e_1 ▸ HEq.refl (B a)) s s_1 h_3)))
+                  (eq_false hcond_1))
+                True.intro))
+        (fun {B_1} {S_1} {s_1 t_1 u} hcond_1 hbody hrest =>
+          Lean.Grind.intro_with_eq (ifThenElse B S T = whileDo B_1 S_1) False
+            (s = s_1 → t = u → h ≍ BigStep.while_true hcond_1 hbody hrest → False)
+            (eq_false' fun h => False.elim (noConfusion_of_Nat Stmt.ctorIdx h)) fun h_2 =>
+            False.casesOn (fun x => s = s_1 → t = u → h ≍ BigStep.while_true hcond_1 hbody hrest → False) h_2)
+        (fun {B_1} {S_1} {s_1} hcond_1 =>
+          Lean.Grind.intro_with_eq (ifThenElse B S T = whileDo B_1 S_1) False
+            (s = s_1 → t = s_1 → h ≍ BigStep.while_false hcond_1 → False)
+            (eq_false' fun h => False.elim (noConfusion_of_Nat Stmt.ctorIdx h)) fun h_2 =>
+            False.casesOn (fun x => s = s_1 → t = s_1 → h ≍ BigStep.while_false hcond_1 → False) h_2)
+        (Eq.refl (ifThenElse B S T)) (Eq.refl s) (Eq.refl t) (HEq.refl h))
+
 theorem if_iff {B S T s t} : (ifThenElse B S T, s) ==> t ↔ (B s ∧ (S, s) ==> t) ∨ (¬ B s ∧ (T, s) ==> t) := by
   grind =>
     instantiate only [BigStep.if_true, BigStep.if_false]
